e07823beb1bd167ec212f6635cb67946a0f22cae
Aliaksandr <108933210+Alexfather121@users.noreply.github.com>
2023-10-22 09:45:49 +0300

    html

--- sites/portfolio/router.php
+++
@@ -1,8 +0,0 @@
-<?php
-$request_path = $_GET['route'];
-if($request_path == '')
-    require $base_path . 'list.php';
-else if ($request_path == 'about')
-    require $base_path . 'about\index.php';
-else if ($request_path == 'contacts')
-    require $base_path . 'contacts\index.php';
